2229b84d08a19b1489a9f2ac8f0087afc6b7eb5a
Tankred <tankreddesign@yahoo.com>
2018-11-16 10:44:42 +0100

    UPDATE cypress - Mocha Chai

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,3 +3,9 @@ describe('My First Test', function() {
     expect(true).to.equal(true)
   })
 })
+
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(false)
+  })
+})
